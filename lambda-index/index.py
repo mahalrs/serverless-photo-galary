2df270f06481d2003570d0c355b456e038c93330
Rajwinder Mahal <rajwinder@mahal.dev>
2022-11-10 03:12:30 -0500

    Implement lambdas

--- lambda-index/index.py
+++ lambda-index/index.py
@@ -21,22 +21,27 @@ def lambda_handler(event, context):
     
     object_key = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
-    timestamp = event['Records'][0]['eventTime']
-    
+
     s3client = boto3.client('s3')
     metadata = s3client.head_object(Bucket=bucket, Key=object_key)
     print(metadata)
-    #
-    # TODO: get custom labels from metadata
-    #
     
+    # timestamp = metadata['ResponseMetadata']['HTTPHeaders']['last-modified']
+    timestamp = event['Records'][0]['eventTime']
+    labels = []
+    
+    if 'x-amz-meta-customLabels' in metadata['ResponseMetadata']['HTTPHeaders']:
+        custom_labels = metadata['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customLabels']
+        #
+        # TODO: split custom labels and add to labels
+        #
+
     rekog = boto3.client('rekognition')
     rek_res = rekog.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': object_key}})
-    labels = rek_res['Labels']
-    print(labels)
-    #
-    # TODO: extract label names from labels
-    #
+    print(rek_res['Labels'])
+    
+    for l in rek_res['Labels']:
+        labels.append(l['Name'].lower())
     
     search = OpenSearch(
         hosts = [{'host': host, 'port': 443}],
@@ -50,15 +55,12 @@ def lambda_handler(event, context):
         'objectKey': object_key,
         'bucket': bucket,
         'createdTimestamp': timestamp,
-        'labels': [
-            'page',
-            'text',
-            'file'
-        ]
+        'labels': labels
     }
+    print(document)
     
-    search.index(index=index, doc_type='_doc', id=timestamp, body=document)
-    print(search.get(index=index, doc_type='_doc', id=timestamp))
+    search.index(index=index, id=timestamp, body=document, refresh=True)
+    print(search.get(index=index, id=timestamp))
     
     message = 'Hello World! LF1 v3.0'
     return message
--- lambda-search/index.py
+++ lambda-search/index.py
@@ -1,11 +1,100 @@
 import json
 import os
 import requests
+import boto3
+
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+
+from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+
+region = 'us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+host = os.environ["PHOTOS_OPENSEARCH_ENDPOINT"]
+index = 'photos'
+
+bot_id = os.environ["LEX_BOT_ID"]
+bot_alias_id = os.environ["LEX_BOT_ALIAS_ID"]
+session_id = 'testuser'
 
 
 def lambda_handler(event, context):
-    print("OpenSearch Endpoint", os.environ["PHOTOS_OPENSEARCH_ENDPOINT"])
-    print("Received event: " + json.dumps(event, indent=2))
+    print("Received event: " + json.dumps(event))
+
+    #
+    # TODO: extract query term from request params
     
-    message = 'Hello World! LF2 v3.0'
-    return message
+    
+    lex_req('dog')
+    query('file')
+    
+    res = {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+            
+        },
+        'body': json.dumps('message from LF2')
+    }
+    
+    # res = {
+    #     'statusCode': 200,
+    #     'headers': {'Content-Type': 'application/json'},
+    #     'body': 'mybody',
+    #     'results': [
+    #         {
+    #             'url': 'url-to-s3',
+    #             'labels': [
+    #                 'label1',
+    #                 'label2'
+    #             ]
+    #         }
+    #     ]
+    # }
+    return res
+
+
+def lex_req(msg):
+    client = boto3.client('lexv2-runtime')
+    
+    res = client.recognize_text(
+            botId=bot_id,
+            botAliasId=bot_alias_id,
+            localeId='en_US',
+            sessionId=session_id,
+            text=msg)
+    
+    msg_from_lex = res.get('messages', [])
+    print(res)
+    print(msg_from_lex)
+
+
+def query(term):
+    q = {
+        "size": 5,
+        "query": {
+            "multi_match": {
+                "query": term
+            }
+        }
+    }
+    
+    search = OpenSearch(
+        hosts = [{'host': host, 'port': 443}],
+        http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    
+    res = search.search(index=index, body=q)
+    print(res)
+    #return data
